74c26b069107063ddbec89c3c201885e3f07935b
LuisJimenez24 <Avanzadox@gmail.com>
2019-09-28 17:24:02 +0200

    Cambiado método de llamada a la creación del cubo

--- inteligentes/src/main/java/proyecto/inteligentes/CubeSolver.java
+++ inteligentes/src/main/java/proyecto/inteligentes/CubeSolver.java
@@ -2,7 +2,6 @@ package proyecto.inteligentes;
 
 import java.io.FileReader;
 import java.io.IOException;
-
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
@@ -11,13 +10,14 @@ public class CubeSolver {
 	public static void main(String[] args) throws ParseException {
 
 		final JSONObject cubo = fileReader();
-		Cube cube = new Cube();
-		cube.setRight(divider(cubo.get("RIGHT").toString().toCharArray()));
-		cube.setBack((divider(cubo.get("BACK").toString().toCharArray())));
-		cube.setFront(((divider(cubo.get("FRONT").toString().toCharArray()))));
-		cube.setLeft(((divider(cubo.get("LEFT").toString().toCharArray()))));
-		cube.setUp(((divider(cubo.get("UP").toString().toCharArray()))));
-		cube.setDown(((divider(cubo.get("DOWN").toString().toCharArray()))));
+		Cube cube = new Cube(
+				divider(cubo.get("BACK").toString().toCharArray()),
+				divider(cubo.get("DOWN").toString().toCharArray()),
+				divider(cubo.get("FRONT").toString().toCharArray()),
+				divider(cubo.get("LEFT").toString().toCharArray()),
+				divider(cubo.get("RIGHT").toString().toCharArray()),
+				divider(cubo.get("UP").toString().toCharArray()));
+		System.out.print(cube);
 	}
 
 	public static JSONObject fileReader() throws ParseException {
