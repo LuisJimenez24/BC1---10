87ce3c517d5d3a87b10f3d67eef6792eff0c7844
LuisJimenez24 <Avanzadox@gmail.com>
2019-09-28 17:17:51 +0200

    Implementado método que divide el JSON y lo añade al objeto tipo Cube

--- inteligentes/src/main/java/proyecto/inteligentes/Cube.java
+++ inteligentes/src/main/java/proyecto/inteligentes/Cube.java
@@ -1,5 +1,73 @@
 package proyecto.inteligentes;
 
 public class Cube {
+	private char[][] back;
+	private char[][] down;
+	private char[][] front;
+	private char[][] left;
+	private char[][] right;
+	private char[][] up;
+
+	public Cube() {
+
+	}
+
+	public Cube(char[][] back, char[][] down, char[][] front, char[][] left, char[][] right, char[][] up) {
+		super();
+		this.back = back;
+		this.down = down;
+		this.front = front;
+		this.left = left;
+		this.right = right;
+		this.up = up;
+	}
+
+	public char[][] getBack() {
+		return back;
+	}
+
+	public void setBack(char[][] back) {
+		this.back = back;
+	}
+
+	public char[][] getDown() {
+		return down;
+	}
+
+	public void setDown(char[][] down) {
+		this.down = down;
+	}
+
+	public char[][] getFront() {
+		return front;
+	}
+
+	public void setFront(char[][] front) {
+		this.front = front;
+	}
+
+	public char[][] getLeft() {
+		return left;
+	}
+
+	public void setLeft(char[][] left) {
+		this.left = left;
+	}
+
+	public char[][] getRight() {
+		return right;
+	}
+
+	public void setRight(char[][] right) {
+		this.right = right;
+	}
+
+	public char[][] getUp() {
+		return up;
+	}
+
+	public void setUp(char[][] up) {
+		this.up = up;
+	}
 
 }
--- inteligentes/src/main/java/proyecto/inteligentes/CubeSolver.java
+++ inteligentes/src/main/java/proyecto/inteligentes/CubeSolver.java
@@ -1,6 +1,5 @@
 package proyecto.inteligentes;
 
-import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 
@@ -8,40 +7,50 @@ import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
 
-
 public class CubeSolver {
 	public static void main(String[] args) throws ParseException {
-		
-		final JSONObject cubo = fileReader();
-		System.out.print(cubo);
 
+		final JSONObject cubo = fileReader();
+		Cube cube = new Cube();
+		cube.setRight(divider(cubo.get("RIGHT").toString().toCharArray()));
+		cube.setBack((divider(cubo.get("BACK").toString().toCharArray())));
+		cube.setFront(((divider(cubo.get("FRONT").toString().toCharArray()))));
+		cube.setLeft(((divider(cubo.get("LEFT").toString().toCharArray()))));
+		cube.setUp(((divider(cubo.get("UP").toString().toCharArray()))));
+		cube.setDown(((divider(cubo.get("DOWN").toString().toCharArray()))));
 	}
 
 	public static JSONObject fileReader() throws ParseException {
-		JSONParser parser = new JSONParser();
+		final JSONParser parser = new JSONParser();
 		JSONObject initCube = new JSONObject();
 		try {
-			Object fileJson = parser.parse(new FileReader("/Users/luisj/Documents/GitHub/BC1---10/inteligentes/JSON.json"));
+			Object fileJson = parser
+					.parse(new FileReader("/Users/luisj/Documents/GitHub/BC1---10/inteligentes/JSON.json"));
 			initCube = (JSONObject) fileJson;
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		
-		
-		/*BufferedReader reader;
-		try {
-			reader = new BufferedReader(
-					new FileReader("/Users/luisj/Documents/GitHub/BC1---10/inteligentes/JSON.json"));
-			String line = reader.readLine();
-			while (line != null) {
-				System.out.println(line);
-				line = reader.readLine();
-			}
-			reader.close();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		*/
 		return initCube;
+	}
+
+	public static char[][] divider(char[] face) {
+
+		char[][] result = new char[3][3];
+		int x = 0;
+		for (int i = 1; i < face.length - 1; i++) {
+			if (face[i] == '[') {
+				int y = 0;
+				i++;
+				while (face[i] != ']') {
+					if (face[i] != ',') {
+						result[x][y] = face[i];
+						++y;
+					}
+					++i;
+				}
+				++x;
+			}
+		}
+		return result;
 	}
 }
