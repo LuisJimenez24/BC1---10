7efba6a9419803f2f911e8a97cac7218be5a0f6b
LuisJimenez24 <Avanzadox@gmail.com>
2019-09-29 12:13:14 +0200

    Las dimensiones del cubo ya no son estáticas

--- inteligentes/src/main/java/proyecto/inteligentes/CubeSolver.java
+++ inteligentes/src/main/java/proyecto/inteligentes/CubeSolver.java
@@ -10,22 +10,18 @@ public class CubeSolver {
 	public static void main(String[] args) throws ParseException {
 
 		final JSONObject cubo = fileReader();
-		Cube cube = new Cube(
-				divider(cubo.get("BACK").toString().toCharArray()),
-				divider(cubo.get("DOWN").toString().toCharArray()),
-				divider(cubo.get("FRONT").toString().toCharArray()),
-				divider(cubo.get("LEFT").toString().toCharArray()),
-				divider(cubo.get("RIGHT").toString().toCharArray()),
+		Cube cube = new Cube(divider(cubo.get("BACK").toString().toCharArray()),
+				divider(cubo.get("DOWN").toString().toCharArray()), divider(cubo.get("FRONT").toString().toCharArray()),
+				divider(cubo.get("LEFT").toString().toCharArray()), divider(cubo.get("RIGHT").toString().toCharArray()),
 				divider(cubo.get("UP").toString().toCharArray()));
-		System.out.print(cube);
 	}
 
 	public static JSONObject fileReader() throws ParseException {
+		/*Método para leer del archivo JSON ubicado en la carpeta del proyecto*/
 		final JSONParser parser = new JSONParser();
 		JSONObject initCube = new JSONObject();
 		try {
-			Object fileJson = parser
-					.parse(new FileReader("/Users/luisj/Documents/GitHub/BC1---10/inteligentes/JSON.json"));
+			Object fileJson = parser.parse(new FileReader("JSON.json"));
 			initCube = (JSONObject) fileJson;
 		} catch (IOException e) {
 			e.printStackTrace();
@@ -34,8 +30,24 @@ public class CubeSolver {
 	}
 
 	public static char[][] divider(char[] face) {
-
-		char[][] result = new char[3][3];
+		/*
+		 * Método para guardar cada número del archivo JSON en una coordinada de la matriz
+		 */
+		int nCounter = 0;
+		for (char c : face) {
+			if (c == '[' || c == ']' || c == ',') {
+			}else {
+				++nCounter; //Variable que guarda cuántos números hay guardados
+			}
+		}
+		int dimensions = 0;
+		for(int i = 1; i<255 ; i++) {
+			if((i * i) == nCounter) {
+				dimensions = i; //Variable que calcula la potencia de la dimensión del cubo
+				break;
+			}
+		}
+		char[][] result = new char[dimensions][dimensions];
 		int x = 0;
 		for (int i = 1; i < face.length - 1; i++) {
 			if (face[i] == '[') {
@@ -43,8 +55,8 @@ public class CubeSolver {
 				i++;
 				while (face[i] != ']') {
 					if (face[i] != ',') {
-						result[x][y] = face[i];
-						++y;
+						result[x][y] = face[i]; //La variable result guarda todos los números según su posición
+						++y;					//en el JSON
 					}
 					++i;
 				}
