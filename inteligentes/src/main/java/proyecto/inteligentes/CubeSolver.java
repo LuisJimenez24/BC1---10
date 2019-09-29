21c144b4edf560e1ca55c2c29812069510e6d54e
LuisJimenez24 <Avanzadox@gmail.com>
2019-09-29 17:12:26 +0200

    Implementado método para calcular la id en md5

--- inteligentes/src/main/java/proyecto/inteligentes/CubeSolver.java
+++ inteligentes/src/main/java/proyecto/inteligentes/CubeSolver.java
@@ -2,9 +2,13 @@ package proyecto.inteligentes;
 
 import java.io.FileReader;
 import java.io.IOException;
+import java.math.BigInteger;
+
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 
 public class CubeSolver {
 	public static void main(String[] args) throws ParseException {
@@ -14,9 +18,10 @@ public class CubeSolver {
 				divider(cubo.get("DOWN").toString().toCharArray()), divider(cubo.get("FRONT").toString().toCharArray()),
 				divider(cubo.get("LEFT").toString().toCharArray()), divider(cubo.get("RIGHT").toString().toCharArray()),
 				divider(cubo.get("UP").toString().toCharArray()));
+		System.out.println ("MD5 "+md5Generator("Hola"));
 	}
 
-	public static JSONObject fileReader() throws ParseException {
+	private static JSONObject fileReader() throws ParseException {
 		/*Método para leer del archivo JSON ubicado en la carpeta del proyecto*/
 		final JSONParser parser = new JSONParser();
 		JSONObject initCube = new JSONObject();
@@ -29,7 +34,7 @@ public class CubeSolver {
 		return initCube;
 	}
 
-	public static char[][] divider(char[] face) {
+	private static char[][] divider(char[] face) {
 		/*
 		 * Método para guardar cada número del archivo JSON en una coordinada de la matriz
 		 */
@@ -65,4 +70,21 @@ public class CubeSolver {
 		}
 		return result;
 	}
+
+	private static String md5Generator(String input) {
+		try {
+			MessageDigest instanceMDigest = MessageDigest.getInstance("MD5");
+			byte [] mDigest = instanceMDigest.digest(input.getBytes());
+			BigInteger number = new BigInteger(1, mDigest);
+			String hashText = number.toString(16);
+			while (hashText.length() < 32) {
+				hashText = "0" + hashText;
+			}
+			return hashText;
+		} catch (NoSuchAlgorithmException e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+	
 }
